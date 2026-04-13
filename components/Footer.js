c6f2f1331072e2c2fb5ae1465e5b34910f44eac7
shrazesh <163748799+shrazesh@users.noreply.github.com>
2026-04-13 14:49:45 +0545

    Added Navbar, Footer, Home design and global styles

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 2026 Shrajesh Shrestha. All rights reserved.</p>
+    </footer>
+  );
+}
---
+++ components/Navbar.js
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return (
+    <nav className="nav">
+      <h2 className="logo">Shrajesh</h2>
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/projects">Projects</a>
+        <a href="/blog">Blog</a>
+      </div>
+    </nav>
+  );
+}
